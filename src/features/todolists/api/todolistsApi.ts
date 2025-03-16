fb53878d85475119b16d4a1aad06777b58f05a09
gkirber <george.kirber@gmail.com>
2025-03-16 16:46:26 +0100

    Complete

---
+++ src/features/todolists/api/todolistsApi.ts
@@ -0,0 +1,18 @@
+import {instance} from '@/common/instance/instance'
+import type {Todolist} from './todolistsApi.types'
+import {BaseResponse} from "@/common/types";
+
+export const todolistsApi = {
+    getTodolists() {
+        return instance.get<Todolist[]>('/todo-lists')
+    },
+    createTodolist(title: string) {
+        return instance.post<BaseResponse<{ item: Todolist }>>('/todo-lists', {title});
+    }, deleteTodolist(id: string) {
+        return instance.delete<BaseResponse>(`/todo-lists/${id}`);
+    },
+    changeTodolistTitle(payload: { id: string, title: string }) {
+        const {title, id} = payload
+        return instance.put<BaseResponse>(`/todo-lists/${id}`, {title})
+    }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from 'path';
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://github.com/gkirber/todolist-11",
   plugins: [react()],
   resolve: {
     alias: {
